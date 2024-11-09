2bb79e7f01a32fc7fcf7f9d41025df7c9cdebc7d
Samuel Gonzalez Morro <samuel.gonzalez.m@itnow.es>
2024-11-09 15:57:45 +0100

    Enable broker connection retry on startup in Celery configuration

--- src/kafka_celery_worker/core/celeryconfig.py
+++ src/kafka_celery_worker/core/celeryconfig.py
@@ -3,3 +3,4 @@ from kafka_celery_worker.core.config import settings
 broker_url = settings.CELERY_BROKER_URL
 result_backend = settings.CELERY_RESULT_BACKEND
 timezone = settings.TIMEZONE
+broker_connection_retry_on_startup = True
